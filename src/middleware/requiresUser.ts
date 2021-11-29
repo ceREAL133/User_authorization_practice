128dd485c7bc9446e10a567f4f5dc5a47e487ed9
ceREAL133 <rlejko13@gmail.com>
2021-11-29 23:19:38 +0200

    fixed bug that allows doing something even if user's session was closed

--- src/middleware/requiresUser.ts
+++ src/middleware/requiresUser.ts
@@ -1,5 +1,6 @@
 import { get } from 'lodash';
 import { Request, Response, NextFunction } from 'express';
+import { findSessions } from '../service/session.service';
 
 const requiresUser = async (
   req: Request,
@@ -7,9 +8,16 @@ const requiresUser = async (
   next: NextFunction,
 ) => {
   const user = get(req, 'user');
+  const sessionId = get(req, 'user.session');
 
   if (!user) {
     return res.sendStatus(403);
+  }
+
+  const isSession = await findSessions({ _id: sessionId, valid: true });
+
+  if (!isSession.length) {
+    return res.sendStatus(401);
   }
 
   return next();
