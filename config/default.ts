04db96569c0469645bc4d53665ab5e1f7eef3bd8
ceREAL133 <rlejko13@gmail.com>
2021-11-29 12:06:31 +0200

    changed .env

--- config/default.ts
+++ config/default.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export default {
   port: 3000,
   host: 'localhost',
-  dbUri: `mongodb+srv://dan:${process.env.ATLAS_PASSWORD}@cluster0.93pq6.mongodb.net/users-database?retryWrites=true&w=majority`,
+  dbUri: process.env.dbUri,
   saltWorkFactor: 10,
   accessTokenTtl: '15m',
   refreshTokenTtl: '1y',
